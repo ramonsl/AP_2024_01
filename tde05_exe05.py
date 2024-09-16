82f26c41d862635befa0a0c74f412e19079ede24
Ramon Lummertz <ramonsl@gmail.com>
2024-09-16 19:48:21 -0300

    add 5

---
+++ tde05_exe05.py
@@ -0,0 +1,4 @@
+''' Faça um algoritmo para ler 
+dois números. Se os números forem iguais, 
+imprimir a mensagem: "NÚMEROS IGUAIS" e encerrar 
+execução; caso contrário, imprimir o de maior valor.'''
