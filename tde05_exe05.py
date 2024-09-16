f7a493555e39949655c715b8becc83f21eb17b6d
Ramon Lummertz <ramonsl@gmail.com>
2024-09-16 19:55:41 -0300

    Atualizar lista
    exercio 05

--- tde05_exe05.py
+++ tde05_exe05.py
@@ -2,3 +2,14 @@
 dois números. Se os números forem iguais, 
 imprimir a mensagem: "NÚMEROS IGUAIS" e encerrar 
 execução; caso contrário, imprimir o de maior valor.'''
+
+numero_01=int(input("Digite um numero 01\n"))
+numero_02=int(input("Digite um numero 02\n"))
+
+if numero_01== numero_02:
+    print ("Numeros Iguais.")
+elif numero_01>numero_02:
+    print (f"O numero maior é {numero_01}")
+else:
+    print (f"O numero maior é {numero_02}")
+ 
